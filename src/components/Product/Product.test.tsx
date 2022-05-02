d55349f7248522c538f61cc7ab8d931e1ab263cd
nacen <nacen.dev@gmail.com>
2022-05-03 02:53:16 +0800

    test: calls handleClick

--- src/components/Product/Product.test.tsx
+++ src/components/Product/Product.test.tsx
@@ -1,18 +1,45 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Product } from "./Product";
+import user from "@testing-library/user-event";
 
-const props = 
-  { img: "image", name: "Salmon Sushi Bake", price: 36}
+const props = {
+  img: "image",
+  name: "Salmon Sushi Bake",
+  price: 36,
+  handleClick: jest.fn(),
+};
 
 describe("Product", () => {
   it("renders the product with price, image, name, order button", () => {
-    render(<Product image={props.img} name={props.name} price={props.price} />);
+    render(
+      <Product
+        image={props.img}
+        name={props.name}
+        price={props.price}
+        handleClick={props.handleClick}
+      />
+    );
 
     expect(screen.getByRole("img")).toHaveAttribute("src", props.img);
     expect(screen.getByText(/36/)).toBeInTheDocument();
     expect(screen.getByText(/Salmon Sushi Bake/)).toBeInTheDocument();
-    expect(screen.getByRole("button", {name: /Add to Cart/i})).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /Add to Cart/i })
+    ).toBeInTheDocument();
   });
-  
+
+  it("calls handleClick function", () => {
+    render(
+      <Product
+        image={props.img}
+        name={props.name}
+        price={props.price}
+        handleClick={props.handleClick}
+      />
+    );
+
+    user.click(screen.getByRole("button", { name: /Add to Cart/i}))
+    expect(props.handleClick).toHaveBeenCalledTimes(1);
+  });
 });
