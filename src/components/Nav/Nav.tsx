a81d4f7dd4a107a751242af5353ea0b45fbb5c73
nacen <nacen.dev@gmail.com>
2022-05-03 02:02:15 +0800

    feat: Navigation

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { MdShoppingCart } from "react-icons/md";
+
+interface Props {}
+
+const Nav = (props: Props) => {
+  return (
+    <nav className="py-2 px-4 flex bg-red-700 text-white items-center">
+      <Link to="/" className="mr-auto text-2xl">Resto</Link>
+      <ul className="flex gap-4 text-xl">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li className="flex gap-2 items-center">
+          <span>Cart</span>
+          <MdShoppingCart />
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export { Nav };
