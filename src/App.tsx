701b315798ac6f7b35b0592d82f70c5e8d36ee3e
nacen <nacen.dev@gmail.com>
2022-05-03 02:14:58 +0800

    feat: render Nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Nav } from "./components/Nav/Nav";
 
 function App() {
   return (
-    <div></div>
+    <div className="grid grid-rows-[10%_90%] grid-cols-1 h-screen">
+      <Nav />
+      <main>
+        <Outlet />
+      </main>
+    </div>
   );
 }
 
