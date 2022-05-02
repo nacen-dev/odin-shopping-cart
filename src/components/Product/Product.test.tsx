da3cbd656de6417a2f17077eb51d3c94f66226fc
nacen <nacen.dev@gmail.com>
2022-05-03 02:37:13 +0800

    refactor: add order button in render

--- src/components/Product/Product.test.tsx
+++ src/components/Product/Product.test.tsx
@@ -6,11 +6,13 @@ const props =
   { img: "image", name: "Salmon Sushi Bake", price: 36}
 
 describe("Product", () => {
-  it("renders the product with price, image, name", () => {
+  it("renders the product with price, image, name, order button", () => {
     render(<Product image={props.img} name={props.name} price={props.price} />);
 
     expect(screen.getByRole("img")).toHaveAttribute("src", props.img);
     expect(screen.getByText(/36/)).toBeInTheDocument();
     expect(screen.getByText(/Salmon Sushi Bake/)).toBeInTheDocument();
+    expect(screen.getByRole("button", {name: /order/i})).toBeInTheDocument();
   });
+  
 });
