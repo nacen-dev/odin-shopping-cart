461c8b13888068b8e00677968169b2328a4bf95d
nacen <nacen.dev@gmail.com>
2022-05-03 02:17:24 +0800

    refactor: remove trailing spaces

--- src/components/Home/Home.test.tsx
+++ src/components/Home/Home.test.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { Home } from './Home';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Home } from "./Home";
 
 describe("Home", () => {
-  it("renders delicious food, and order now button ", () => {
+  it("renders delicious food, and order now button", () => {
     render(<Home />);
 
     expect(screen.getByRole("heading")).toHaveTextContent("Delicious Food");
-    expect(screen.getByRole("button", {name: /Order Now/i})).toBeInTheDocument();
-  })
-})
+    expect(
+      screen.getByRole("button", { name: /Order Now/i })
+    ).toBeInTheDocument();
+  });
+});
