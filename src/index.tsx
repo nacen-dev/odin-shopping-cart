62cc2d0c16aec8f8777025efb0aaad1b5541ad0b
nacen <nacen.dev@gmail.com>
2022-05-03 02:13:38 +0800

    feat: routing components

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home/Home";
+import { ProductList } from "./components/ProductList/ProductList";
+import { productListData } from "./productsData";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,7 +14,15 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route
+            path="menu"
+            element={<ProductList productList={productListData} />}
+          />
+        </Route>
+      </Routes>
     </Router>
   </React.StrictMode>
 );
