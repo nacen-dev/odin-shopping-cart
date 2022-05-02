813710be58e862d048707b5be71c917504517bfa
nacen <nacen.dev@gmail.com>
2022-05-03 02:04:38 +0800

    test: renders h1 and button

---
+++ src/components/Home/Home.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Home } from './Home';
+
+describe("Home", () => {
+  it("renders delicious food, and order now button ", () => {
+    render(<Home />);
+
+    expect(screen.getByRole("heading")).toHaveTextContent("Delicious Food");
+    expect(screen.getByRole("button", {name: /Order Now/i})).toBeInTheDocument();
+  })
+})
