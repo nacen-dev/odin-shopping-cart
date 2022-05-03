5c91c1599056e32a593199a4c5c599dac055fdc9
nacen <nacen.dev@gmail.com>
2022-05-03 23:18:45 +0800

    refactor: pass in id prop

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -7,13 +7,15 @@ interface Props {
 
 const ProductList = ({ productList }: Props) => {
   return (
-    <ul className="grid grid-cols-3 gap-4">
+    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {productList.map((product) => (
         <li key={product.name}>
           <Product
             image={product.image}
             name={product.name}
             price={product.price}
+            handleClick={() => {}}
+            id={product.id}
           />
         </li>
       ))}
