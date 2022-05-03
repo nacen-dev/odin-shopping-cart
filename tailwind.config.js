2069d88d92f9e2475ff50f20a345f9241d410719
nacen <nacen.dev@gmail.com>
2022-05-04 00:29:53 +0800

    add: black-rgba

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "black-rgba": "rgba(0, 0, 0, 0.7)",
+      },
+    },
   },
   plugins: [],
-}
+};
