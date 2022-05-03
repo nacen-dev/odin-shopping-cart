a19719f192a098de5e843f485836b8c789bd2480
nacen <nacen.dev@gmail.com>
2022-05-04 02:43:07 +0800

    refactor: use cartContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Outlet } from "react-router-dom";
+import { Cart } from "./components/Cart/Cart";
 import { Nav } from "./components/Nav/Nav";
+import { SideDrawer } from "./components/SideDrawer/SideDrawer";
+import { CartContext } from "./context/CartContext";
 
 function App() {
+  const { showCart, closeCart, openCart } = useContext(CartContext);
+
   return (
     <div className="grid grid-rows-[10%_90%] grid-cols-1 h-screen">
-      <Nav />
+      <Nav showCart={openCart} />
       <main>
         <Outlet />
       </main>
+      <SideDrawer open={showCart} onClose={closeCart}>
+        <Cart />
+      </SideDrawer>
     </div>
   );
 }
