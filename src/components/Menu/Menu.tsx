1b796da1ca69294f50a495b5bf320973ea81e95a
nacen <nacen.dev@gmail.com>
2022-05-04 02:38:16 +0800

    refactor: remove CartContextProvider

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { CartContextProvider } from "../../context/CartContext";
 import { productListData } from "../../productsData";
 import { ProductList } from "../ProductList/ProductList";
 
@@ -7,14 +6,12 @@ interface Props {}
 
 const Menu = (props: Props) => {
   return (
-    <CartContextProvider>
-      <div className="bg-red-600 p-4">
-        <h2 className="text-white text-center text-2xl m-4 font-semibold">
-          Menu
-        </h2>
-        <ProductList productList={productListData} />
-      </div>
-    </CartContextProvider>
+    <div className="bg-red-600 p-4">
+      <h2 className="text-white text-center text-2xl m-4 font-semibold">
+        Menu
+      </h2>
+      <ProductList productList={productListData} />
+    </div>
   );
 };
 
