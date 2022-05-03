a6e86b17bb9b5d407601e410c4b029dfa17a9843
nacen <nacen.dev@gmail.com>
2022-05-04 02:40:35 +0800

    feat: create SideDrawer

---
+++ src/components/SideDrawer/SideDrawer.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { MdClose } from "react-icons/md";
+import { IModal, Modal } from "../Modal/Modal";
+import styles from "./SideDrawer.module.css"
+
+interface Props extends IModal {}
+
+const SideDrawer = ({ onClose, open, className, children }: Props) => {
+  return (
+    <Modal
+      onClose={onClose}
+      open={open}
+      className={`right-0 top-0 h-full overflow-y-scroll w-[250px] p-4 rounded-none flex flex-col
+       ${styles.sideDrawer} ${className}`}
+    >
+        <button
+          className="flex items-center text-xl self-end mb-4"
+          onClick={onClose}
+        >
+          Close
+          <MdClose />
+        </button>
+      {children}
+    </Modal>
+  );
+};
+
+export { SideDrawer };
