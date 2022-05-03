4b4c311e9ada1aea19ef0d141d02169dcd07ec80
nacen <nacen.dev@gmail.com>
2022-05-03 22:58:21 +0800

    refactor: Add Link to route to menu

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-
-interface Props {
-
-};
+import HomeBackground from "../../assets/home-bg.jpg";
+import { Link } from "react-router-dom";
+interface Props {}
 
 const Home = (props: Props) => {
   return (
-    <div className="flex items-center justify-center flex-col gap-4">
-      <h1>Delicious Food</h1>
-      <button>Order Now</button>
+    <div
+      className="flex items-center justify-center flex-col gap-4 h-full bg"
+      style={{
+        background: `linear-gradient(
+        to right,
+        rgba(0, 0, 0, 0.6),
+        rgba(0, 0, 0, 0.6)
+      ),url(${HomeBackground})`,
+      }}
+    >
+      <h1 className="text-4xl text-white">Delicious Food</h1>
+      <button className="text-lg rounded py-4 px-6 bg-red-600 text-white">
+        <Link to="/menu">Order Now</Link>
+      </button>
     </div>
-  )
+  );
 };
 
-export { Home };  
+export { Home };
